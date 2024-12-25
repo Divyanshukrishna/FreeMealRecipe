5a7ca62e04fa11dcf7f183b4f332f727723f26b7
Divyanshu <divyanshukrishna11@gmail.com>
2024-12-26 00:08:52 +0530

    heading-added

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -3,30 +3,39 @@ import MainCards from './MainCards';
 
 const MainPage = () => {
     const [data,setData] = useState();
-    const [search,setSearch] = useState();
-
+    const [search,setSearch] = useState("");
+    const [message,setMessage] = useState("");
     const handleInput = (event) =>{
         setSearch(event.target.value);
     }
 
     const myFun =async () =>{
-        const get = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
-        const jsonData = await get.json();
-        // console.log(jsonData.meals);
-        setData(jsonData.meals);
+        if(search == ""){
+            setMessage("Please enter something")
+        }else{
+            const get = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
+            const jsonData = await get.json();
+            // console.log(jsonData.meals);
+            setData(jsonData.meals);
+            setMessage("");
+        }
     }
 
 
   return (
+    <>
+    <h1 className='head'>FOOD RECIPE APP</h1>
     <div className='container'>
         <div className="searchBar">
             <input type="text" placeholder='Enter Dish' onChange={handleInput}  />
             <button onClick={myFun}>Search</button>
         </div>
+        <h3 className='error'>{message}</h3>
         <div>
             <MainCards details={data} />
         </div>
     </div>
+    </>
   )
 }
 
